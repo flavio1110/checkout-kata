d8f497aa74943237d8025fc8945c2fd7f80026ea
Flávio Silva <flavio1110@gmail.com>
2017-08-13 00:38:50 +0200

    implement promotion calculation

--- src/CheckoutKata.Classic/CheckoutService.cs
+++ src/CheckoutKata.Classic/CheckoutService.cs
@@ -26,13 +26,17 @@ namespace CheckoutKata.Classic
 
         private IEnumerable<CheckoutItem> GetItems(IEnumerable<SelectedProduct> selection, IEnumerable<Product> products)
         {
-            foreach (var product in products)
+            foreach (var product in products.Where(p => selection.Select(sel => sel.Id).Contains(p.Id)))
+            {
+                var quantity = selection.FirstOrDefault(s => s.Id == product.Id).Quantity;
                 yield return new CheckoutItem 
                 {
                     Product = product,
-                    Price = product.Price * selection.FirstOrDefault(s => s.Id == product.Id).Quantity
+                    Price = product.Promotion != null 
+                        ? product.Promotion.CalculatePrice(product.Price, quantity)
+                        : product.Price * quantity
                 };
+            }
         }
-
     }
 }
--- test/CheckoutKata.Classic.Tests/CheckoutServiceTests.cs
+++ test/CheckoutKata.Classic.Tests/CheckoutServiceTests.cs
@@ -24,11 +24,12 @@ namespace CheckoutKata.Classic.Tests
             var products = new List<Product>
             {
                 new Product { Id = 1,  Name = "t-shirt", Promotion = null, Price = 2f },
-                new Product { Id = 2, Name = "hat", Promotion = null, Price = 5f },
+                new Product { Id = 2, Name = "hat", Promotion = null, Price = 6f },
             };
             
             repositoryMock.Setup(r => r.ListProductsByIds(It.IsAny<IEnumerable<int>>()))
-                .Returns(products);
+                .Returns(products)
+                .Verifiable();
 
             var selection = new List<SelectedProduct>
             {
@@ -47,8 +48,46 @@ namespace CheckoutKata.Classic.Tests
             actual.Items[1].Product.Id.ShouldBe(2);
             actual.Items[1].Product.Name.ShouldBe("hat");
             actual.Items[1].Product.Promotion.ShouldBeNull();
+            actual.Items[1].Product.Price.ShouldBe(6f);
+            actual.Items[1].Price.ShouldBe(12f);
+
+            repositoryMock.Verify();
+        }
+
+        [Fact]
+        public void GetCheckoutInfo_WithProductsWithPromotion_ShouldReturnPriceWithPromotionApplied()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1,  Name = "t-shirt", Promotion = new SecondFree(), Price = 2f },
+                new Product { Id = 2, Name = "hat", Promotion = new GetThreePayTen(), Price = 5f },
+            };
+            
+            repositoryMock.Setup(r => r.ListProductsByIds(It.IsAny<IEnumerable<int>>()))
+                .Returns(products)
+                .Verifiable();
+
+            var selection = new List<SelectedProduct>
+            {
+                new SelectedProduct { Id = 1, Quantity = 2 },
+                new SelectedProduct { Id = 2, Quantity = 3 },
+            };
+
+            var actual = sut.GetCheckoutInfo(selection);
+
+            actual.Items[0].Product.Id.ShouldBe(1);
+            actual.Items[0].Product.Name.ShouldBe("t-shirt");
+            actual.Items[0].Product.Promotion.ShouldBeOfType<SecondFree>();
+            actual.Items[0].Product.Price.ShouldBe(2f);
+            actual.Items[0].Price.ShouldBe(2f);
+
+            actual.Items[1].Product.Id.ShouldBe(2);
+            actual.Items[1].Product.Name.ShouldBe("hat");
+            actual.Items[1].Product.Promotion.ShouldBeOfType<GetThreePayTen>();
             actual.Items[1].Product.Price.ShouldBe(5f);
             actual.Items[1].Price.ShouldBe(10f);
+
+            repositoryMock.Verify();
         }
     }
 }
